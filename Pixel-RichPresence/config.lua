77257b85d2fce8ae61dcdead8e3c9217c120ab52
Pixel <100500099+PixelTheDeveloper@users.noreply.github.com>
2022-02-27 03:42:50 +0100

    Add files via upload

---
+++ Pixel-RichPresence/config.lua
@@ -0,0 +1,10 @@
+Config = {}
+Config.ClientID = 886649991874113576 -- Put your discord bot client id here
+Config.PlayerCount = 1024
+Config.PlayerText = "Players" -- Player text. Example Players 10/32
+Config.ResourceTimer = 5 -- Time in witch resource updates in seconds
+Config.UseESXIdentity = true -- Uses ESX Identity name not steam name
+Config.Buttons = { -- Your buttons
+    {index = 0,name = 'Pixel - Discord',url = 'https://discord.gg/fJekXq5jUP'},
+    {index = 1,name = 'Pixel - Connect',url = 'fivem://connect/127.0.0.1:30120'}
+}
