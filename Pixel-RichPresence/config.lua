82c15ea81ccc619e99e41b40a47ade0508c9fcc9
Pixel <100500099+PixelTheDeveloper@users.noreply.github.com>
2022-04-12 21:34:27 +0200

    Update config.lua

--- Pixel-RichPresence/config.lua
+++ Pixel-RichPresence/config.lua
@@ -1,5 +1,5 @@
 Config = {}
-Config.ClientID = 886649991874113576 -- Put your discord bot client id here
+Config.ClientID = 960913771725987900 -- Put your discord bot client id here
 Config.PlayerCount = 1024
 Config.PlayerText = "Players" -- Player text. Example Players 10/32
 Config.ResourceTimer = 5 -- Time in witch resource updates in seconds
